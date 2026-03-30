98d29410cc5f777c2f7d45a1996be6cebeababa2
jratul <jratul@users.noreply.github.com>
2026-03-30 13:54:11 +0900

    fix: tailwind-merge가 커스텀 text-body1 등을 color로 오인해 text-white 제거하는 버그 수정
    extendTailwindMerge로 커스텀 font-size 클래스 등록

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,5 +1,21 @@
 import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      "font-size": [
+        "text-display1",
+        "text-title1",
+        "text-title2",
+        "text-title3",
+        "text-body1",
+        "text-body2",
+        "text-caption1",
+      ],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
